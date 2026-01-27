4a519468e77a41f33bad24a08b50670b7676c27e
Antonio Mac Mini <srdolar@me.com>
2026-01-27 20:23:01 +0100

    Initial Commit

---
+++ DocPilot/DocPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocPilotApp.swift
+//  DocPilot
+//
+//  Created by Antonio Muñoz on 27/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DocPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
