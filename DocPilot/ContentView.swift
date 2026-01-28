c2e0c0c2429a77cbde2490c578cd31d299d007c0
Antonio Mac Mini <srdolar@me.com>
2026-01-28 19:18:44 +0100

    Make camera open only on button tap

--- DocPilot/ContentView.swift
+++ DocPilot/ContentView.swift
@@ -10,14 +10,10 @@ import Vision
 #if canImport(UIKit)
 import UIKit
 #endif
-#if canImport(VisionKit)
-import VisionKit
-#endif
 
 struct ContentView: View {
     @EnvironmentObject private var store: DocumentStore
     @State private var recognizedText = ""
-    @State private var isShowingScanner = false
     @State private var isShowingCamera = false
     @State private var isProcessing = false
     @State private var errorMessage: String?
@@ -26,15 +22,6 @@ struct ContentView: View {
         VStack(spacing: 16) {
             Text("OCR de documentos")
                 .font(.title2)
-
-#if canImport(VisionKit)
-            Button("Escanear documento") {
-                isShowingScanner = true
-            }
-            .buttonStyle(.borderedProminent)
-#else
-            Text("Escaner no disponible en esta plataforma.")
-#endif
 
 #if canImport(UIKit)
             Button("Capturar foto y OCR") {
@@ -57,18 +44,6 @@ struct ContentView: View {
                 .border(.gray.opacity(0.4))
         }
         .padding()
-#if canImport(VisionKit)
-        .sheet(isPresented: $isShowingScanner) {
-            DocumentScannerView { result in
-                switch result {
-                case .success(let images):
-                    recognizeText(from: images)
-                case .failure(let error):
-                    errorMessage = error.localizedDescription
-                }
-            }
-        }
-#endif
 #if canImport(UIKit)
         .sheet(isPresented: $isShowingCamera) {
             CameraPicker { result in
@@ -167,50 +142,6 @@ struct CameraPicker: UIViewControllerRepresentable {
 
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             picker.dismiss(animated: true)
-        }
-    }
-}
-#endif
-
-#if canImport(VisionKit)
-struct DocumentScannerView: UIViewControllerRepresentable {
-    var completion: (Result<[UIImage], Error>) -> Void
-
-    func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
-        let controller = VNDocumentCameraViewController()
-        controller.delegate = context.coordinator
-        return controller
-    }
-
-    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {}
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(completion: completion)
-    }
-
-    final class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
-        private let completion: (Result<[UIImage], Error>) -> Void
-
-        init(completion: @escaping (Result<[UIImage], Error>) -> Void) {
-            self.completion = completion
-        }
-
-        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
-            var images: [UIImage] = []
-            for index in 0..<scan.pageCount {
-                images.append(scan.imageOfPage(at: index))
-            }
-            controller.dismiss(animated: true)
-            completion(.success(images))
-        }
-
-        func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
-            controller.dismiss(animated: true)
-        }
-
-        func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
-            controller.dismiss(animated: true)
-            completion(.failure(error))
         }
     }
 }
